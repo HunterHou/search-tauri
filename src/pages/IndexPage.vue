2653fe9e4502def628e18febcedfb61e2575be99
hunter <183598821@qq.com>
2023-10-21 22:45:41 +0800

    1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,76 +8,82 @@
         <q-btn color="green" class="q-ml-md" label="刷新" icon="refresh" @click="f5" />
       </template>
     </q-banner>
-    <q-splitter v-model="splitterModel">
-      <template v-slot:before>
-        <div class="q-pa-md">
-          <div class="text-h5 q-mb-md">标签分布</div>
-          <div class="q-gutter-sm" style="
+    <div v-if="tableData.length == 0 && tagData.length == 0 && scanTime.length == 0">
+      <h3>数据分析中ing</h3>
+    </div>
+    <div class="main" v-else>
+      <q-splitter v-model="splitterModel">
+        <template v-slot:before>
+          <div class="q-pa-md">
+            <div class="text-h5 q-mb-md">标签分布</div>
+            <div class="q-gutter-sm" style="
               display: flex;
               flex-direction: row;
               flex-wrap: wrap;
               justify-content: flex-start;
             ">
-            <div v-for="tag in tagData" :key="tag" style="width: auto">
-              <q-btn color="secondary" class="btn-fixed-width" v-if="tag.Cnt > 1" @click="folderGotoMenu(tag.Name)">
-                {{ `${tag.Name} (${tag.Cnt})` }}
-                <q-badge color="orange" floating>{{ tag.SizeStr }}</q-badge>
-              </q-btn>
-            </div>
-          </div>
-        </div>
-      </template>
-
-      <template v-slot:after>
-        <q-splitter v-model="insideModel" horizontal>
-          <template v-slot:before>
-            <div class="q-pa-md">
-              <div class="text-h5 q-mb-md">扫描分析</div>
-              <div class="row justify-center q-gutter-sm">
-                <q-card class="my-card" v-for="sc in scanTime" :key="sc">
-                  <q-card-section>
-                    <div class="text-h6">{{ sc.Name }}</div>
-                    <div class="text-subtitle2">时间：{{ sc.Cnt }}</div>
-                    <div class="text-subtitle2">文件数：{{ sc.Size }}</div>
-                  </q-card-section>
-                  <q-separator />
-                  <q-card-actions vertical>
-                    <q-btn flat color="blue" @click="gotoMenu({ ...sc, IsDir: true })">传送</q-btn>
-                  </q-card-actions>
-                </q-card>
+              <div v-for="tag in tagData" :key="tag" style="width: auto">
+                <q-btn color="secondary" class="btn-fixed-width" v-if="tag.Cnt > 1" @click="folderGotoMenu(tag.Name)">
+                  {{ `${tag.Name} (${tag.Cnt})` }}
+                  <q-badge color="orange" floating>{{ tag.SizeStr }}</q-badge>
+                </q-btn>
               </div>
             </div>
-          </template>
+          </div>
+        </template>
 
-          <template v-slot:after>
-            <div class="q-pa-md">
-              <div class="text-h5 q-mb-md">文件分析</div>
-              <div class="row justify-center q-gutter-sm">
-                <q-card class="my-card" v-for="sc in tableData" :key="sc">
-                  <q-card-section>
-                    <div class="text-h6">{{ sc.Name }}</div>
-                    <div class="text-subtitle2">文件数：{{ sc.Cnt }}</div>
-                    <div class="text-subtitle2">文件大小：{{ sc.SizeStr }}</div>
-                  </q-card-section>
-                  <q-separator />
-                  <q-card-actions vertical>
-                    <div style="
+        <template v-slot:after>
+          <q-splitter v-model="insideModel" horizontal>
+            <template v-slot:before>
+              <div class="q-pa-md">
+                <div class="text-h5 q-mb-md">扫描分析</div>
+                <div class="row justify-center q-gutter-sm">
+                  <q-card class="my-card" v-for="sc in scanTime" :key="sc">
+                    <q-card-section>
+                      <div class="text-h6">{{ sc.Name }}</div>
+                      <div class="text-subtitle2">时间：{{ sc.Cnt }}</div>
+                      <div class="text-subtitle2">文件数：{{ sc.Size }}</div>
+                    </q-card-section>
+                    <q-separator />
+                    <q-card-actions vertical>
+                      <q-btn flat color="blue" @click="gotoMenu({ ...sc, IsDir: true })">传送</q-btn>
+                    </q-card-actions>
+                  </q-card>
+                </div>
+              </div>
+            </template>
+
+            <template v-slot:after>
+              <div class="q-pa-md">
+                <div class="text-h5 q-mb-md">文件分析</div>
+                <div class="row justify-center q-gutter-sm">
+                  <q-card class="my-card" v-for="sc in tableData" :key="sc">
+                    <q-card-section>
+                      <div class="text-h6">{{ sc.Name }}</div>
+                      <div class="text-subtitle2">文件数：{{ sc.Cnt }}</div>
+                      <div class="text-subtitle2">文件大小：{{ sc.SizeStr }}</div>
+                    </q-card-section>
+                    <q-separator />
+                    <q-card-actions vertical>
+                      <div style="
                         display: flex;
                         flex-direction: row;
                         justify-content: space-around;
                       ">
-                      <q-btn color="blue" flat v-if="!sc.IsDir" @click="gotoMenu(sc)">传送</q-btn>
-                      <q-btn color="blue" flat v-if="sc.IsDir" @click="openThis(sc)">打开</q-btn>
-                      <q-btn color="red" flat v-if="sc.IsDir" @click="deleteThis(sc)">删除</q-btn>
-                    </div>
-                  </q-card-actions>
-                </q-card>
+                        <q-btn color="blue" flat v-if="!sc.IsDir" @click="gotoMenu(sc)">传送</q-btn>
+                        <q-btn color="blue" flat v-if="sc.IsDir" @click="openThis(sc)">打开</q-btn>
+                        <q-btn color="red" flat v-if="sc.IsDir" @click="deleteThis(sc)">删除</q-btn>
+                      </div>
+                    </q-card-actions>
+                  </q-card>
+                </div>
               </div>
-            </div>
-          </template>
-        </q-splitter>
-      </template>
-    </q-splitter>
+            </template>
+          </q-splitter>
+        </template>
+      </q-splitter>
+    </div>
+
   </div>
 </template>
 
@@ -157,19 +163,19 @@ const openThis = async (data) => {
   const { Name } = data;
   const res = await OpenFolerByPath({ dirpath: Name });
   if (res.Code == 200) {
-    $q.notify({ type: 'positive', message: '执行成功' , multiLine: true, position: 'bottom-right' });
+    $q.notify({ type: 'positive', message: '执行成功', multiLine: true, position: 'bottom-right' });
   } else {
-    $q.notify({ type: 'warning', message: '执行失败' , multiLine: true, position: 'bottom-right' });
+    $q.notify({ type: 'warning', message: '执行失败', multiLine: true, position: 'bottom-right' });
   }
 };
 const deleteThis = async (data) => {
   const { Name } = data;
   const res = await DeleteFolerByPath({ dirpath: Name });
   if (res.Code == 200) {
-    $q.notify({ type: 'positive', message: '执行成功' , multiLine: true, position: 'bottom-right' });
+    $q.notify({ type: 'positive', message: '执行成功', multiLine: true, position: 'bottom-right' });
     refreshIndex();
   } else {
-    $q.notify({ type: 'warning', message: '执行失败' , multiLine: true, position: 'bottom-right' });
+    $q.notify({ type: 'warning', message: '执行失败', multiLine: true, position: 'bottom-right' });
   }
 };
 const refreshIndex = async () => {
