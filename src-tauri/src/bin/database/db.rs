8ee65ba6b021251a34f258f467fd1998c0e7159e
hunter <183598821@qq.com>
2023-10-25 00:50:04 +0800

    1

--- src-tauri/src/bin/database/db.rs
+++ src-tauri/src/bin/database/db.rs
@@ -1,26 +1,26 @@
-// use rusqlite::{Connection};
+use rusqlite::{Connection,NO_PARAMS};
 
-// pub fn db_connection() -> rusqlite::Connection {
-//     println!("db_connection...");
-//     Connection::open("./search.db").unwrap()
-// }
+fn db_connection() -> Connection {
+    println!("db_connection...");
+    Connection::open("./search.db").unwrap()
+}
 
-// pub fn init_db() {
-//     let conn = db_connection();
-//     println!("Initializing table t_file");
-//     match conn.execute("drop table if exists t_file", []) {
-//         Ok(val) => println!("drop success :{}", val),
-//         Err(err) => println!("drop Fail:{}", err),
-//     };
-//     match conn.execute("create  table t_file(
-//         id integer primary key autoincrement,
-//         code text,
-//         name text,
-//         path text,
-//         size integer,
-//         sizeStr text
-//     )", []) {
-//         Ok(val) => println!("create Fail:{}", val),
-//         Err(err) => println!("create Fail:{}", err),
-//     };
-// }
+pub fn init_db() {
+    let conn = db_connection();
+    println!("Initializing table t_file");
+    match conn.execute("drop table if exists t_file", NO_PARAMS) {
+        Ok(val) => println!("drop success :{}", val),
+        Err(err) => println!("drop Fail:{}", err),
+    };
+    match conn.execute("create  table t_file(
+        id integer primary key autoincrement,
+        code text,
+        name text,
+        path text,
+        size integer,
+        sizeStr text
+    )", NO_PARAMS) {
+        Ok(val) => println!("create Fail:{}", val),
+        Err(err) => println!("create Fail:{}", err),
+    };
+}
--- src-tauri/src/bin/utils/do_file_name.rs
+++ src-tauri/src/bin/utils/do_file_name.rs
@@ -42,7 +42,7 @@ pub fn actress_from_name(name: &str) -> String {
         let item1 = str;
         let index = item1.find("-");
         if index.is_none() && str.len() > 0 {
-            println!("actress_from_name :{} ",&str[0..str.len()-1] );
+            // println!("actress_from_name :{} ",&str[0..str.len()-1] );
             return String::from(&str[0..str.len()-1]);
         }
         continue;
