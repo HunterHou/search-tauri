df77631bd9c578213913bdb622bc5254e45aa98b
harmay <hunter.hou@harmay.com>
2023-10-24 15:59:46 +0800

    1

---
+++ src-tauri/src/bin/database/db.rs
@@ -0,0 +1,17 @@
+use rusqlite::{Connection};
+
+pub fn db_connection() -> rusqlite::Connection {
+    println!("db_connection...");
+    Connection::open("./search.db").unwrap();
+}
+pub fn init_db() {
+    let conn = db_connection();
+    println!("Initializing table t_file");
+    let _ = conn.execute("create or replace table t_file(
+        id integer primary key autoincrement,
+        name text,
+        path text,
+        size integer,
+        sizeStr text
+    )", []);
+}
--- src-tauri/src/bin/main.rs
+++ src-tauri/src/bin/main.rs
@@ -1,4 +1,5 @@
-
+mod database;
+use crate::database::db::init_db;
 fn main() {
-    
+    init_db();
 }
