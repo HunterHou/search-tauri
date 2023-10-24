d8be4aa10c436b7f69c11bee5b6b7428547cb3c0
harmay <hunter.hou@harmay.com>
2023-10-24 18:22:22 +0800

    1

--- src-tauri/src/bin/database/db.rs
+++ src-tauri/src/bin/database/db.rs
@@ -4,14 +4,22 @@ pub fn db_connection() -> rusqlite::Connection {
     println!("db_connection...");
     Connection::open("./search.db").unwrap()
 }
+
 pub fn init_db() {
     let conn = db_connection();
     println!("Initializing table t_file");
-    let _ = conn.execute("create or replace table t_file(
+    let res = match conn.execute("create or replace table t_file(
         id integer primary key autoincrement,
         name text,
         path text,
         size integer,
         sizeStr text
-    )", []);
+    )", []) {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}", err);
+            0
+        },
+    };
+    println!("success:{}", res)
 }
